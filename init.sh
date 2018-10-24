665bf1c1fb8fd35a156ed85eec0802a3564bf9b3
mt-coff <mt.coff.00@gmail.com>
2018-10-25 00:54:49 +0900

    fix init script

--- init.sh
+++ init.sh
@@ -1,116 +1,24 @@
 #!/bin/bash
 
-# vim
-while true; do
-  echo -n "Do you use vim? [y/n]:"
-  read ans
-  if [ -z $ans ] ; then
-    continue
-  elif [ $ans = 'y' ] || [ $ans = 'yes' ] ; then
-    ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-    ln -snf ~/dotfiles/vim ~/.vim
-    break
-  elif [ $ans = 'n' ] || [ $ans = 'no' ]; then
-    echo "Skipped!"
-    break
-  else
-    continue
-  fi
-done
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-# tmux
-while true; do
-  echo -n "Do you use tmux? [y/n]:"
-  read ans
-  if [ -z $ans ] ; then
-    continue
-  elif [ $ans = 'y' ] || [ $ans = 'yes' ] ; then
-    ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-    break
-  elif [ $ans = 'n' ] || [ $ans = 'no' ]; then
-    echo "Skipped!"
-    break
-  else
-    continue
-  fi
-done
+ln -sf $SCRIPT_DIR/vim/vimrc ~/.vimrc
+ln -snf $SCRIPT_DIR/vim ~/.vim
+ln -sf $SCRIPT_DIR/tmux/tmux.conf ~/.tmux.conf
+ln -snf $SCRIPT_DIR/nvim ~/.config/nvim
+ln -sf $SCRIPT_DIR/zsh/zlogin ~/.zlogin
+ln -sf $SCRIPT_DIR/zsh/zlogout ~/.zlogout
+ln -sf $SCRIPT_DIR/zsh/zshenv ~/.zshenv
+ln -sf $SCRIPT_DIR/zsh/zprofile ~/.zprofile
+ln -sf $SCRIPT_DIR/zsh/zshrc ~/.zshrc
+ln -sf $SCRIPT_DIR/commons/gitignore_global ~/.gitignore_global
+ln -sf $SCRIPT_DIR/commons/editrc ~/.editrc
 
-# neovim
-while true; do
-  echo -n "Do you use neovim? [y/n]:"
-  read ans
-  if [ -z $ans ] ; then
-    continue
-  elif [ $ans = 'y' ] || [ $ans = 'yes' ] ; then
-    ln -snf ~/dotfiles/nvim ~/.config/nvim
-    break
-  elif [ $ans = 'n' ] || [ $ans = 'no' ]; then
-    echo "Skipped!"
-    break
-  else
-    continue
-  fi
-done
-
-# tern.js
-while true; do
-  echo -n "Do you use tern.js? [y/n]:"
-  read ans
-  if [ -z $ans ] ; then
-    continue
-  elif [ $ans = 'y' ] || [ $ans = 'yes' ] ; then
-    ln -sf ~/dotfiles/commons/tern-config ~/.tern-config
-    break
-  elif [ $ans = 'n' ] || [ $ans = 'no' ]; then
-    echo "Skipped!"
-    break
-  else
-    continue
-  fi
-done
-
-# zsh
-while true; do
-  echo -n "Do you use zsh? [y/n]:"
-  read ans
-  if [ -z $ans ] ; then
-    continue
-  elif [ $ans = 'y' ] || [ $ans = 'yes' ] ; then
-    ln -sf ~/dotfiles/zsh/zlogin ~/.zlogin
-    ln -sf ~/dotfiles/zsh/zlogout ~/.zlogout
-    ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
-    ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
-    ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
-    break
-  elif [ $ans = 'n' ] || [ $ans = 'no' ]; then
-    echo "Skipped!"
-    break
-  else
-    continue
-  fi
-done
-
-# anyenv
-while true; do
-  echo -n "Do you use anyenv? [y/n]"
-  read ans
-  if [ -z $ans ] ; then
-    continue
-  elif [ $ans = 'y' ] || [ $ans = 'yes' ] ; then
-    git clone https://github.com/riywo/anyenv ~/.anyenv
-    git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
-    break
-  elif [ $ans = 'n' ] || [ $ans = 'no' ]; then
-    echo "Skipped!"
-    break
-  else
-    continue
-  fi
-done
-
-# global gitignore
-ln -sf ~/dotfiles/commons/gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
-# editrc
-ln -sf ~/dotfiles/commons/editrc ~/.editrc
+if [[ ! -d ~/.anyenv ]]; then
+  git clone https://github.com/riywo/anyenv ~/.anyenv
+  git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
+fi
+
+echo "Initialization has ended."
