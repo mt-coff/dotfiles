5a4995a00bec97732a40da916e69d3819df19607
mt-coff <mt.coff.00@gmail.com>
2018-11-01 14:09:50 +0900

    update tmux conf & install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
 if [ -n $DOTROOT ]; then
   if [ $(uname) = 'Darwin' ]; then
-    brew bundle
+    if !(type brew > /dev/null 2>&1); then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      brew bundle $DOTROOT/Brewfile
+    fi
   fi
 fi
 
 
-if [[ ! -d ~/.anyenv ]]; then
+if [ ! -d ~/.anyenv ]; then
   git clone https://github.com/riywo/anyenv ~/.anyenv
   git clone https://github.com/znz/anyenv-update.git ~/.anyenv/plugins/anyenv-update
+fi
+
+if [ ! -d ~/.tmux ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
